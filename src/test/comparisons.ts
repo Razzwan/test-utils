1607e45176b9ca612731607e2321e7e928e881fe
Razzwan <razvanlomov@gmail.com>
2023-10-20 12:19:49 +0300

    Add gas comparing to test utils

--- src/test/comparisons.ts
+++ src/test/comparisons.ts
@@ -24,3 +24,33 @@ export function compareSliceForTest(subject: any, cmp: Slice): CompareResult {
         negMessage: ((subject: any, cmp: Slice) => `Expected\n${subject}\nNOT to equal\n${cmp}\nbut it does`).bind(undefined, subject, cmp),
     }
 }
+
+
+export function gasUsage(messageResult: any): bigint {
+    try {
+        return messageResult.transactions.reduce((gas, tx) => {
+            return gas + tx.totalFees.coins;
+        }, 0n)
+    } catch (e) {
+        console.info("Compared object is not SendMessageResult");
+        return 0n
+    }
+
+}
+
+export function gasCompare(messageResult: any, toCompare: bigint, accuracy: bigint = 2n): boolean {
+    const gas = gasUsage(messageResult);
+    if (gas >= toCompare - accuracy) {
+        if (gas <= toCompare + accuracy) {
+            return true;
+        }
+    }
+    return false;
+}
+export function gasUsageForTest(subject: any, cmp: bigint, accuracy: bigint = 2n): CompareResult {
+    return {
+        pass: gasCompare(subject, cmp, accuracy),
+        posMessage: ((subject: any, cmp: bigint, accuracy?: bigint) => `Expected\ngas usage of message transactions\nto equal\n${cmp}${accuracy === 0n ? '' : `±${accuracy}`}`).bind(undefined, subject, cmp, accuracy),
+        negMessage: ((subject: any, cmp: bigint, accuracy?: bigint) => `Expected\ngas usage of message transactions\nNOT to equal\n${cmp}${accuracy === 0n ? '' : `±${accuracy}`}\nbut it does`).bind(undefined, subject, cmp, accuracy),
+    }
+}
